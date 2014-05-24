af5b896ca9e13fb542ca0131c43a92a6206e7c92
Heather Amler <heatheramler@yahoo.com>
2014-05-24 08:25:18 -0400

    Initial commit to new branch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,70 +1,74 @@
 
-// constructor that makes dice objects
+// constructor that makes first instance roll
 function Dice1 (sides) {
 	this.diceSides = parseInt(sides);
 	this.roll = function () {
 		return result = (Math.floor(Math.random() * sides) + 1)
-		console.log(result)
 	};
 	this.score = this.roll();	
 };
 
+// constructor that makes second instance roll
 function Dice2 (sides) {
 	this.diceSides = parseInt(sides);
 	this.roll = function () {
 		return result = (Math.floor(Math.random() * sides) + 1)
-		console.log(result)
 	};
 	this.score = this.roll();	
 };
 
+// constructor that makes third instance roll
 function Dice3 (sides) {
 	this.diceSides = parseInt(sides);
 	this.roll = function () {
 		return result = (Math.floor(Math.random() * sides) + 1)
-		console.log(result)
 	};
 	this.score = this.roll();	
 };
 
 //defines function to compare dice
+
 function rollCompare () {
-	if (player.score > computer.score) {
+
+	if (playerScore > computerScore) {
 		outcome = "VICTORY!";
 	}
-	else if (player.score < computer.score) {
+	else if (playerScore < computerScore) {
 		outcome = "FAILURE!";
 	}
-	else if (player.score == computer.score) {
+	else if (playerScore == computerScore) {
 		outcome = "ITS A TIE!";
 	}
 };
+
 
 
 //creates dice
 $('.submit').click(function(){
 	var sides = $('.dice-sides').val();
 //makes new player and computer dice and scores
-	player = new Dice1(sides);
+	player1 = new Dice1(sides);
+	computer1 = new Dice1(sides);
 	
-	computer = new Dice1(sides);
-	
-	player = new Dice2(sides);
-	
-	computer = new Dice2(sides);
+	player2 = new Dice2(sides);
+	computer2 = new Dice2(sides);
 
-	player = new Dice3(sides);
-	
-	computer = new Dice3(sides);
-	
+	player3 = new Dice3(sides);
+	computer3 = new Dice3(sides);
 
 
 //calls dice comparison function
 	rollCompare();
-//displays scores
-$('.answer').html('player score: ' + player.score +'<br>computer score: ' + computer.score + '<br> ' + outcome);
-//tells you who won
-rollCompare();
+
+
+// Displays player roll
+	$('.player-roll1').html('player score: ' + player1.score);
+	$('.player-roll2').html('player score: ' + player2.score);
+	$('.player-roll3').html('player score: ' + player3.score);
+
+// Displays computer roll
+	$('.computer-roll1').html('computer score: ' + computer1.score);
+	$('.computer-roll2').html('computer score: ' + computer2.score);
+	$('.computer-roll3').html('computer score: ' + computer3.score);
+
 })
-
-
